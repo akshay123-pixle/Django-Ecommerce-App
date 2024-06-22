0f2bc35f9de9dd72de3896f1ab250830c5160ef7
Akshay <ajadhao9696@gmail.com>
2024-06-22 21:56:53 +0530

    added DB password

--- ecom/settings.py
+++ ecom/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'PASSWORD': os.environ['DB_PASSWORD_YO'],
         'HOST': 'roundhouse.proxy.rlwy.net',
         'PORT': '25194',
         
